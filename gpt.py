148ff4ea8e00bbe85ef811cc8a5f2a8140cfac76
Leonard Püttmann <leopuettmann@gmail.com>
2024-07-22 21:37:35 +0200

    First train run and downloaded gpt 2 weights

--- gpt.py
+++ gpt.py
@@ -1,6 +1,7 @@
-
+import tiktoken
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader 
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,  # Vocabulary size
@@ -11,6 +12,40 @@ GPT_CONFIG_124M = {
     "drop_rate": 0.1,     # Dropout rate
     "qkv_bias": False     # Query-Key-Value bias
 }
+
+def create_dataloader_v1(text, batch_size=4, max_length=256, 
+                         stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(text, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset, 
+        batch_size=batch_size, 
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=0
+    )
+    return dataloader
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, text, tokenizer, max_length, stride): 
+        self.input_ids = []
+        self.target_ids = []
+        
+        token_ids = tokenizer.encode(text)
+        
+        for i in range(0, len(token_ids) - max_length, stride): 
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+            
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+   
     
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, 
